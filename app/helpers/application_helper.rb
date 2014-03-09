87b41ad76f768789b628a55c2a6aa3590c8f8b58
blaok <ssrs1994@126.com>
2014-03-09 16:39:18 +0800

    修正helper在未登录时罢工的bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
 
   def admin0?(user)
-    0==user.admin
+    user and (0==user.admin)
   end
 
   def admin1?(user)
-    0==user.admin or 1==user.admin
+    user and (0==user.admin or 1==user.admin)
   end
 
 end
